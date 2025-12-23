e6cf556c06f214f3ca2df4618e68c067d05677b8
Sangharsh <sangharshpipare@gmail.com>
2025-12-23 21:34:26 +0530

    Added the description for the solved problem

--- binarySearchTree/searchInBinarySearchTree/Solution.java
+++ binarySearchTree/searchInBinarySearchTree/Solution.java
@@ -1,18 +1,6 @@
 package binarySearchTree.searchInBinarySearchTree;
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
  */
 
 class TreeNode{
@@ -33,7 +21,7 @@ class TreeNode{
 }
 
 /**
- *
+ * This is an recursive approach for the solution, its simple we check if the current node has the value we are looking for if yes then return that node , or else depending on the property of Binary search tree we observe if target is smaller than current node's value then we need to move to left side of the tree and perform the same checks. Thus we recall the same function with the left node as the root node, if the match is found then fine else conditions are checked if the target is greater then it will trace the right side of tree till it reaches. if result is not found then it is clear that the result does not exist, we can return null or the current node's value. 
  */
 public class Solution{
 
