0b5fafc315621402a9e50472bd012984b64a99bc
Sangharsh <sangharshpipare001@gmail.com>
2025-12-23 21:20:55 +0530

    Testing things, added a question

---
+++ binarySearchTree/deleteNodeInBST/Solution.java
@@ -0,0 +1,10 @@
+package binarySearchTree.deleteNodeInBST;
+
+import javax.swing.tree.TreeNode;
+
+public class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return root;
+    }
+}
+
